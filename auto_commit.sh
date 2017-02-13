38db869be6809683d633c35aa805fa1f0c65156b
Jackson Dean <jckson.dean@gmail.com>
2017-02-13 09:22:48 -0800

    update commit script

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,5 +2,9 @@
 
 cd $HOME/dotfiles && \
   git add --all && \
-  git commit -m "Auto commit" && \
+  git commit -m "Cron auto commit" && \
+  git branch tmp-master && \
+  git fetch --all && \
+  git reset --hard origin/master
+  git merge -X theirs tmp-master && \
   git push origin master

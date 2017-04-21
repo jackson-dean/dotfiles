1f9f6a5b9c77d5384b6fe95abeb5a3e7dd75c7e2
Jackson Dean <jckson.dean@gmail.com>
2017-04-21 09:03:28 -0700

    Cron auto commit

--- auto_commit.sh
+++ auto_commit.sh
@@ -11,15 +11,19 @@ cd $HOME/dotfiles && \
   git commit -m "Cron auto commit" && \
 
   # Create a temp copy of our local master
-  git branch $TMP_BRANCH && \
+  git branch $TMP_BRANCH
+
+  # branch creation failing intermittently for some reason. Exit with error if this happens
+  if [[ ! `git branch --list $TMP_BRANCH` ]]; then
+    echo "Failed creating branch $TMP_BRANCH"
+    exit 1
+  fi
 
   # Fetch remote changes
   git fetch --all && \
 
   # Force local master to mirror remote master
-  git reset --hard origin/master
-
-  [[ `git branch --list $TMP_BRANCH` ]] && echo 'BRANCH EXISTS!!!!'
+  git reset --hard origin/master && \
 
   # Merge in our local master copy, favoring our local changes for merge conflict resolution strategy
   git merge -m "Merging tmp-master" -X theirs $TMP_BRANCH && \

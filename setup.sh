0424df9ca30206c090d14134818673425caf3fcd
Jackson Dean <jckson.dean@gmail.com>
2022-01-22 09:39:44 -0800

    auto install vim plug

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ done
 
 ln -sfv "$PWD/git-completion.bash" "$HOME/git-completion.bash"
 ln -sfv "$PWD/git-prompt.sh" "$HOME/git-prompt.sh"
+
+# install vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
